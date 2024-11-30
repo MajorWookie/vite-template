06e6bd1ab953812fcc8a5a11d87cf3fa5f8aab54
Adrian Anderson <109745656+MajorWookie@users.noreply.github.com>
2024-11-30 04:48:26 -0500

    Add Rich Text Editor component and update dependencies

--- src/pages/Home.page.tsx
+++ src/pages/Home.page.tsx
@@ -1,11 +1,13 @@
 import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
 import { Welcome } from '../components/Welcome/Welcome';
+import { RichTextEditorDemo } from '../components/Demos/RichTextEditor'
 
 export function HomePage() {
   return (
     <>
       <Welcome />
       <ColorSchemeToggle />
+      <RichTextEditorDemo />
     </>
   );
 }
